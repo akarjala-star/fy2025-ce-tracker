5fe26ba773dfa970b7b4275c08d971362069f949
akarjala-star <akarjala@gmail.com>
2026-05-14 12:02:26 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
-#!/bin/sh#!/ required environment variables are present
+#!/bin/sh
+set -e
+
 : "${BASIC_AUTH_USER:?Missing BASIC_AUTH_USER}"
 : "${BASIC_AUTH_PASS:?Missing BASIC_AUTH_PASS}"
 
-# Generate htpasswd file at container startup
 htpasswd -bc /etc/nginx/.htpasswd "$BASIC_AUTH_USER" "$BASIC_AUTH_PASS"
 
-# Start nginx in the foreground
 exec nginx -g 'daemon off;'
-set -e
