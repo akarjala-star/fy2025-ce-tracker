ea84d797ec4c785a6e78927cbc37ac03381766de
akarjala-star <akarjala@gmail.com>
2026-05-14 11:48:43 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
+
 : "${BASIC_AUTH_USER:?Missing BASIC_AUTH_USER}"
-: "${BASIC_AUTH_PASS:?Missing BASIC_AUTH_PASS}"
+: "${BASIC_AUTH_PASS:?Missing BASIC_AUTH
 
 # Generate htpasswd at container startup (always fresh)
 htpasswd -bc /etc/nginx/.htpasswd "$BASIC_AUTH_USER" "$BASIC_AUTH_PASS"
